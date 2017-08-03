e245e6bd5bf473f1a617b00e0f7cb0917b386eb2
Subham Saraf <ssaraf@tavisca.com>
2017-08-03 20:16:57 +0530

    Added the Properties as Per the Questions.

---
+++ Assigments/BikersClubEnrollment/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BikersClubEnrollment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+
+    public class Person
+    {
+        public Name Name { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public double Age{ get { return (DateTime.Now.DayOfYear - DateOfBirth.DayOfYear);} }
+        public Gender Gender { get; set; }
+       
+
+    }
+
+    public class Name
+    {
+        public string Salutation { get; set;}
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+    }
+
+    public enum Gender
+    {
+        Male,
+        Female,
+        TransGender
+    }
+}
