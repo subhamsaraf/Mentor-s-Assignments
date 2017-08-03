06b432c4b379ae9687d779266615c2a3dd1e1075
Subham Saraf <ssaraf@tavisca.com>
2017-08-03 23:21:34 +0530

    Structure of the Program Developed Without Refactoring

--- Assigments/BikersClubEnrollment/Program.cs
+++ Assigments/BikersClubEnrollment/Program.cs
@@ -10,6 +10,19 @@ namespace BikersClubEnrollment
     {
         static void Main(string[] args)
         {
+            BikeClub bikeclub = new BikeClub();
+            Console.WriteLine("How many members do you want to add : ");
+            int members = Convert.ToInt32(Console.ReadLine());
+            Person[] person = new Person[members];
+            for (int i = 0; i < members; i++)
+            {
+                person[i].Name.FirstName = Console.ReadLine();
+                person[i].Name.MiddleName = Console.ReadLine();
+                person[i].Name.LastName = Console.ReadLine();
+                person[i].DateOfBirth = Convert.ToDateTime(Console.ReadLine());
+                person[i].Gender = ConvertConsole.ReadLine();
+
+            }
         }
     }
 
@@ -18,10 +31,15 @@ namespace BikersClubEnrollment
     {
         public Name Name { get; set; }
         public DateTime DateOfBirth { get; set; }
-        public double Age{ get { return (DateTime.Now.DayOfYear - DateOfBirth.DayOfYear);} }
+        public double Age{ get { return (DateTime.Today.Year - DateOfBirth.Year);} }
         public Gender Gender { get; set; }
-       
 
+        public override string ToString()
+        {
+            string Details = $"{Name.Salutation}. {Name.FirstName} {Name.MiddleName} {Name.LastName}, {Gender}, Age {Age}";
+
+            return base.ToString();
+        }
     }
 
     public class Name
@@ -38,4 +56,36 @@ namespace BikersClubEnrollment
         Female,
         TransGender
     }
+
+    public class Club
+    {
+        public List<Person> Member { get; set; }
+        public void Enroll(Person person)
+        {
+            Member.Add(person);
+        }
+            
+    }
+
+    public class BikeClub : Club
+    {
+        
+        public void EnrollInBikeClub(Person person)
+        {
+            if(person.Age>=21)
+            {
+                this.Enroll(person);
+            }
+            
+        }
+
+        public void DisplayMembers()
+        {
+            foreach (var person in this.Member)
+            {
+                Member.ToString();
+            }
+        }
+    }
+    
 }
